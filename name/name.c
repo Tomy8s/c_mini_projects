ae8b2e199b4da271c92d258614e97354e8ef8b2d
Thomas Yates <tom.yates@cordantgroup.com>
2017-12-06 19:13:20 +0000

    changed name to use fgets instead of gets

--- name/name.c
+++ name/name.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   char name[10];
   printf("Please enter your name\n>");
-  gets(name);
+  fgets(name, 10, stdin);
+  name[strlen(name)-1] = 0;
   printf("Hi %s!\n", name);
 };
