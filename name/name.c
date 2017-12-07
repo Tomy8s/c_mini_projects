930174653e51a2ec90bd9255cee811421255a3b5
Thomas Yates <tom.yates@cordantgroup.com>
2017-12-07 18:35:17 +0000

    use strlen to remove \n from end of input

--- name/name.c
+++ name/name.c
@@ -2,10 +2,10 @@
 #include <string.h>
 
 int main() {
-  int name_length_plus_1 = 4;
+  int name_length_plus_1 = 20;
   char name[name_length_plus_1];
   printf("Please enter your name\n>");
   fgets(name, name_length_plus_1, stdin);
-  name[name_length_plus_1 - 1] = 0;
+  name[strlen(name) - 1] = 0;
   printf("Hi %s!\n", name);
 };
